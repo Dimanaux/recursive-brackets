a144d3926081a9b3dde54b82780c5d9b81be734d
Dmitry Barskov <barskoidim@gmail.com>
2019-11-11 18:46:59 +0300

    Correct BracketsSequence to pass tests

--- src/main/java/com/example/brackets/BracketsSequence.java
+++ src/main/java/com/example/brackets/BracketsSequence.java
@@ -1,7 +1,17 @@
 package com.example.brackets;
 
 public class BracketsSequence {
-    private static boolean isCorrectBracketsSequence(String sequence) {
+    private String sequence;
+
+    public BracketsSequence(String sequence) {
+        this.sequence = takeOnlyBrackets(sequence);
+    }
+
+    public boolean isCorrect() {
+        return isCorrect(sequence);
+    }
+
+    private static boolean isCorrect(String sequence) {
         if (sequence.isEmpty()) {
             return true;
         }
@@ -10,8 +20,8 @@ public class BracketsSequence {
         if (pairPosition == -1) {
             return false;
         }
-        return isCorrectBracketsSequence(sequence.substring(1, pairPosition - 1))
-                && isCorrectBracketsSequence(sequence.substring(pairPosition));
+        return isCorrect(sequence.substring(1, pairPosition - 1))
+                && isCorrect(sequence.substring(pairPosition));
     }
 
     private static int findPair(String sequence, char openBracket) {
@@ -26,6 +36,26 @@ public class BracketsSequence {
             i++;
         }
         return depth != 0 ? -1 : i;
+    }
+
+    private String takeOnlyBrackets(String sequence) {
+        StringBuilder brackets = new StringBuilder();
+        for (int i = 0; i < sequence.length(); i++) {
+            if (isBracket(sequence.charAt(i))) {
+                brackets.append(sequence.charAt(i));
+            }
+        }
+        return brackets.toString();
+    }
+
+    private static boolean isBracket(char symbol) {
+        switch (symbol) {
+            case '(': case ')':
+            case '[': case ']':
+            case '{': case '}':
+                return true;
+        }
+        return false;
     }
 
     private static boolean matches(char openingBracket, char closingBracket) {
